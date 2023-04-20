f195d98f42fa42bb2bf9e334305b9e2f8569f085
metal-face <hughesbryan3000@gmail.com>
2023-04-19 23:41:42 -0400

    prettier imported and configured

--- client/src/plugins/vuetify.ts
+++ client/src/plugins/vuetify.ts
@@ -5,22 +5,38 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+import { md3 } from "vuetify/blueprints";
 
 // Composables
-import { createVuetify } from 'vuetify'
-
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
-  theme: {
-    themes: {
-      light: {
-        colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
+    blueprint: md3,
+    icons: {
+        defaultSet: "mdi",
+        aliases,
+        sets: {
+            mdi,
         },
-      },
     },
-  },
-})
+    theme: {
+        defaultTheme: "dark",
+        themes: {
+            light: {
+                colors: {
+                    primary: "#1867C0",
+                    secondary: "#5CBBF6",
+                },
+            },
+            dark: {
+                colors: {
+                    primary: "#1e1e1e",
+                    secondary: "#4e4e4e",
+                },
+            },
+        },
+    },
+});
