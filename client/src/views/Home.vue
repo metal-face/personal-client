38203392d01089ef3af753edf4ed712dd3cc01fa
metal-face <captain@pirateradio.boats>
2023-04-22 16:25:45 -0400

    Features/Improvements
    My name, title and clock were written into the template.
    
    The clock's functionality was written. Date-fns was
    imported to make the current date human readable.
    
    Custom fonts applied to template.

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -4,11 +4,21 @@
             <v-card
                 flat
                 color="transparent"
-                class="fill-height d-flex justify-center align-center"
-                width="100%"
-            >
-                <v-card class="d-flex justify-center align-center" height="100%" width="50%">
-                    <v-card-title> Yes </v-card-title>
+                class="fill-height d-flex align-center justify-center"
+                width="100%">
+                <v-card
+                    flat
+                    color="transparent"
+                    class="d-flex flex-column justify-center align-center"
+                    height="100%"
+                    width="50%">
+                    <v-card flat color="transparent" class="text-center">
+                        <v-card-title class="page-title text-xl-h1"> Bryan Hughes </v-card-title>
+                        <v-card-subtitle class="page-title">
+                            Full-Stack Software Developer
+                        </v-card-subtitle>
+                        <v-card-text class="page-clock"> {{ templateDate }} </v-card-text>
+                    </v-card>
                 </v-card>
             </v-card>
         </v-col>
@@ -16,7 +26,36 @@
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from "vue";
+import { ref, onMounted } from "vue";
+import { format } from "date-fns";
+
+onMounted(() => {
+    createClock();
+});
+
+let templateDate = ref<string>("");
+
+function updateCurrentTime(): void {
+    templateDate.value = formatHumanReadableDate(Date.now());
+}
+
+function createClock(): number {
+    return window.setInterval(() => {
+        updateCurrentTime();
+    }, 1000);
+}
+
+function formatHumanReadableDate(current: number): string {
+    return format(current, "PPPPpp");
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.page-clock {
+    font-family: "Source Code Pro", monospace !important;
+}
+
+.page-title {
+    font-family: "Playfair Display", serif !important;
+}
+</style>
