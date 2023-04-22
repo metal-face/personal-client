1879c62ecb09ce2b9887468690c2916000ca15cb
metal-face <hughesbryan3000@gmail.com>
2023-04-21 23:27:12 -0400

    Improvements
    Vuetify grid system implemented in component.

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
-    <v-card>
-        <v-card-text> YEs </v-card-text>
-    </v-card>
+    <v-row class="fill-height">
+        <v-col cols="12">
+            <v-card
+                flat
+                color="transparent"
+                class="fill-height d-flex justify-center align-center"
+                width="100%"
+            >
+                <v-card class="d-flex justify-center align-center" height="100%" width="50%">
+                    <v-card-title> Yes </v-card-title>
+                </v-card>
+            </v-card>
+        </v-col>
+    </v-row>
 </template>
 
-<script>
-export default {
-    name: "UserHome",
-};
+<script setup lang="ts">
+import { reactive, ref } from "vue";
 </script>
 
 <style scoped></style>
