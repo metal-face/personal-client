804e3818c385c659257e9897f87047747594b3e6
metal-face <captain@pirateradio.boats>
2023-04-23 23:04:39 -0400

    Circle Loader composable written.

---
+++ client/src/components/CircleLoader.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-overlay :value="props.loading" absolute>
+        <v-progress-circular :color="props.circleColor" indeterminate :size="128" />
+    </v-overlay>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+interface Props {
+    loading: boolean;
+    circleColor?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    loading: false,
+    circleColor: "purple",
+});
+</script>
+
+<style scoped></style>
