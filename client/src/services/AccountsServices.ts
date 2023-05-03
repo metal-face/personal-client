83a67d5b8ab90078bb771454e10c79684bc77843
metal-face <captain@pirateradio.boats>
2023-05-03 01:40:21 -0400

    Account network services written
    CRUD methods, create, list by id, list many (paginated), update and delete by id.

---
+++ client/src/services/AccountsServices.ts
@@ -0,0 +1,54 @@
+import ApiClient from "@/clients/ApiClient";
+import { AxiosResponse } from "axios";
+
+export default {
+    createAccount(
+        username: string,
+        email: string,
+        password: string,
+    ): Promise<AxiosResponse<any, any>> {
+        return ApiClient({
+            method: "POST",
+            url: "/accounts",
+            data: {
+                username: username,
+                email: email,
+                password: password,
+            },
+        });
+    },
+    fetchAccountById(accountId: string): Promise<AxiosResponse<any, any>> {
+        return ApiClient({
+            method: "GET",
+            url: `/accounts/${accountId}`,
+        });
+    },
+    fetchManyAccounts(page: number, pageSize: number): Promise<AxiosResponse<any, any>> {
+        return ApiClient({
+            method: "GET",
+            url: `/accounts?page=${page}&page_size=${pageSize}`,
+        });
+    },
+    updateAccountById(
+        accountId: string,
+        username?: string,
+        email?: string,
+        password?: string,
+    ): Promise<AxiosResponse<any, any>> {
+        return ApiClient({
+            method: "PATCH",
+            url: `/accounts/${accountId}`,
+            data: {
+                username: username,
+                email: email,
+                password: password,
+            },
+        });
+    },
+    deleteAccountById(accountId: string): Promise<AxiosResponse<any, any>> {
+        return ApiClient({
+            method: "DELETE",
+            url: `/accounts/${accountId}`,
+        });
+    },
+};
