706fe23ac9a41c254666e0c89f8c26f0922c7f7f
metal-face <captain@pirateradio.boats>
2023-05-03 01:41:36 -0400

    Prop definition reverted

--- client/src/components/CircleLoader.vue
+++ client/src/components/CircleLoader.vue
@@ -1,23 +1,17 @@
 <template>
-    <v-overlay :model-value="props.loading" absolute contained persistent class="align-enter justify-center">
+    <v-overlay :model-value="props.loading" absolute persistent class="align-enter justify-center">
         <v-progress-circular :color="props.circleColor" indeterminate :size="128" :width="1" />
     </v-overlay>
 </template>
 
 <script setup lang="ts">
-const props = defineProps({
-    loading: {type: Boolean, required: true, default: false},
-    circleColor: {type: String, required: false, default: "red"}
+interface Props {
+    loading: boolean;
+    circleColor: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    loading: false,
+    circleColor: "purple",
 });
-
-//  interface Props {
-//     loading: boolean;
-//     circleColor: string;
-// }
-
-// const props = withDefaults(defineProps<Props>(), {
-//     loading: false,
-//     circleColor: "purple",
-// });
 </script>
-
