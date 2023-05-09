27b729704b4878ce6ff5a296fda0d230472a49de
metal-face <hughesbryan3000@gmail.com>
2023-05-09 00:43:26 -0400

    prettier formatting and scrim color adjusted

--- client/src/components/CircleLoader.vue
+++ client/src/components/CircleLoader.vue
@@ -1,5 +1,10 @@
 <template>
-    <v-overlay :model-value="props.loading" absolute persistent class="align-enter justify-center">
+    <v-overlay
+        :model-value="props.loading"
+        scrim="rgba(0,0,0,0.5)"
+        absolute
+        persistent
+        class="align-enter justify-center">
         <v-progress-circular :color="props.circleColor" indeterminate :size="128" :width="1" />
     </v-overlay>
 </template>
