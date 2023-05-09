d5083eba64ed1508d7198c8a4c0aa7b1132d6c92
metal-face <hughesbryan3000@gmail.com>
2023-05-09 00:42:43 -0400

    Home page built out further, email field added

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import { ref, onUnmounted, onMounted } from "vue";
 import { format } from "date-fns";
-import CircleLoader from "@/components/CircleLoader.vue";
+// import CircleLoader from "@/components/CircleLoader.vue";
 
 const intervalID = ref<number>(0);
 const templateDate = ref<string>(format(Date.now(), "PPPpp"));
 const loading = ref<boolean>(true);
+const userEmail = ref<string>("");
 
 function createClock() {
     intervalID.value = window.setInterval(() => {
@@ -27,24 +28,42 @@ onUnmounted(() => {
 
 <template>
     <v-row class="fill-height">
-        <CircleLoader v-model:loading="loading" />
         <v-col cols="12">
             <v-card
-            flat
-            color="transparent"
-            class="fill-height d-flex flex-column justify-center align-center"
-            width="100%">
+                flat
+                color="transparent"
+                class="d-flex flex-column justify-center align-center"
+                height="90%"
+                width="100%">
+                <!-- <CircleLoader
+                    :loading="true"
+                    circleColor="pink"
+                    class="d-flex justify-center align-center" /> -->
+                <v-card color="transparent" flat>
                     <v-card-title class="page-title ma-1">
-                        <h1>
-                            Bryan Hughes
-                        </h1>
+                        <h1 class="text-center">Bryan Hughes</h1>
                     </v-card-title>
-                    <v-card-subtitle class="page-title ma-1">
-                        <h3>
-                            Full-Stack Software Developer
-                        </h3>
+                    <v-card-subtitle class="page-clock ma-1">
+                        <h3 class="text-center">Full-Stack Software Developer</h3>
+                        <h3 class="text-center page-clock ma-1">{{ templateDate }}</h3>
                     </v-card-subtitle>
-                    <h4 class="page-clock ma-1">{{ templateDate }}</h4>
+                    <v-card color="transparent" flat class="mt-12">
+                        <label for="email" class="page-clock d-flex justify-center">
+                            Enter your email
+                        </label>
+                        <v-text-field
+                            id="email"
+                            v-model="userEmail"
+                            variant="underlined"
+                            class="page-clock"
+                            append-inner-icon="mdi-email" />
+                    </v-card>
+                </v-card>
+            </v-card>
+            <v-card height="auto" flat color="transparent" width="100%">
+                <v-card-subtitle class="text-center">
+                    <h4 class="text-center page-clock">Made with 🖤 by Metal-Face</h4>
+                </v-card-subtitle>
             </v-card>
         </v-col>
     </v-row>
@@ -55,6 +74,6 @@ onUnmounted(() => {
 }
 
 .page-title {
-    font-family: "Playfair Display", serif !important;
+    font-family: "Calistoga", cursive !important;
 }
 </style>
