dfa943d724d523616c50ff48980b13b7c26f1b6d
metal-face <hughesbryan3000@gmail.com>
2023-05-11 18:05:51 -0400

    Components given contents so they'll load

---
+++ client/src/views/Login.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-card>
+        <v-card-title>Login</v-card-title>
+    </v-card>
+</template>
+<script setup lang="ts"></script>
+<style scoped></style>
---
+++ client/src/views/Signup.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-card>
+        <v-card-title>Signup</v-card-title>
+    </v-card>
+</template>
+<script setup lang="ts"></script>
+<style scoped></style>
