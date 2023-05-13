fe0bc78a3cfc3988ad55e87c002cfd4981d1dbe9
metal-face <hughesbryan3000@gmail.com>
2023-05-13 02:18:28 -0400

    Improvements/Fixes
    components provided props so the email can be passed from the home page.

--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -1,7 +1,18 @@
 <template>
     <v-card>
         <v-card-title>Signup</v-card-title>
+        <v-card-subtitle>{{ props.email }}</v-card-subtitle>
     </v-card>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+interface Props {
+    email?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    email: "",
+});
+</script>
 <style scoped></style>
