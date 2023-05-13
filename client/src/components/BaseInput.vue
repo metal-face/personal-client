7abbd48ab3a4fec6cc912fe6319a18753820c857
metal-face <hughesbryan3000@gmail.com>
2023-05-13 19:32:14 -0400

    Form Input Composable Written

---
+++ client/src/components/BaseInput.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-card flat>
+        <v-text-field
+            v-model="input"
+            :label="props.label"
+            :required="props.required"
+            @input="onInput" />
+    </v-card>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+interface Props {
+    label?: string;
+    required: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    label: "",
+    required: false,
+});
+
+const input = ref<string>("");
+
+const emit = defineEmits<{
+    (e: "input", value: string): void;
+}>();
+
+function onInput() {
+    emit("input", input.value);
+}
+</script>
+
+<style scoped></style>
