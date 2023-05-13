b2518b940dce39839483bc67393b25d90740fd1b
metal-face <hughesbryan3000@gmail.com>
2023-05-13 19:32:36 -0400

    Form Composable Started

---
+++ client/src/components/BaseForm.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-container fluid>
+        <v-row dense>
+            <v-col cols="12">
+                <v-form> </v-form>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
