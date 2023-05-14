429f1d3b9e782fac6af4a71c56b7fa9b131d4bd5
metal-face <hughesbryan3000@gmail.com>
2023-05-14 01:49:34 -0400

    state management utilized

---
+++ client/src/views/Register.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-row justify="center">
+        <v-col cols="6">
+            <v-card height="100%" flat>
+                <v-card-title class="text-center"> Register </v-card-title>
+                <v-form>
+                    <v-text-field v-if="!email" :value="email" disabled />
+                    <v-text-field v-else v-model="email" />
+                </v-form>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useAppStore } from "@/store/app";
+import { onMounted } from "vue";
+
+const store = useAppStore();
+
+onMounted(() => {
+    email.value = store.userEmail;
+});
+
+const email = ref<string>("");
+</script>
+<style scoped></style>
