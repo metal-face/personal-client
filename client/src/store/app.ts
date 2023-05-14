fb7233d45788d2cafcfdc4907ac50de10c7e9915
metal-face <hughesbryan3000@gmail.com>
2023-05-14 01:48:10 -0400

    state management added for user

--- client/src/store/app.ts
+++ client/src/store/app.ts
@@ -1,8 +1,20 @@
 // Utilities
 import { defineStore } from "pinia";
 
+interface State {
+    userEmail: string
+}
+
 export const useAppStore = defineStore("app", {
-    state: () => ({
-        //
+    state: (): State => ({
+        userEmail: "",
     }),
+    getters: {
+        userEmail: (state: State) => state.userEmail,
+    },
+    actions: {
+        setUserEmail(email: string): void {
+            this.userEmail = email;
+        }
+    }
 });
