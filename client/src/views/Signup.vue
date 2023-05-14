8b30db2eb64a4913e0fb95cfb1a066d2d7906757
metal-face <hughesbryan3000@gmail.com>
2023-05-13 23:46:22 -0400

    Signup form started

--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -1,13 +1,24 @@
 <template>
-    <v-card>
-        <v-card-title>Signup</v-card-title>
-        <v-card-subtitle>{{ props.email }}</v-card-subtitle>
-        <BaseInput />
-    </v-card>
+    <v-row justify="center">
+        <v-col cols="6">
+            <v-card height="100%" flat>
+                <v-card-title class="text-center"> Signup </v-card-title>
+                <BaseForm>
+                    <BaseInput
+                        class="ma-2"
+                        label="Email"
+                        :counter="254"
+                        variant="outlined"
+                        placeholder="example@gmail.com" />
+                </BaseForm>
+            </v-card>
+        </v-col>
+    </v-row>
 </template>
 <script setup lang="ts">
 import { defineProps } from "vue";
 import BaseInput from "@/components/BaseInput.vue";
+import BaseForm from "@/components/BaseForm.vue";
 
 interface Props {
     email?: string;
