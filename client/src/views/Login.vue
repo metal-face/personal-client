ed4c2efe7202493a6826a068d8ceef37b716b19e
metal-face <hughesbryan3000@gmail.com>
2023-05-14 01:49:00 -0400

    old props ripped out, will be replaced by state management layer

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -2,19 +2,11 @@
     <v-card>
         <v-card-title>Login</v-card-title>
         <v-card-subtitle>
-            {{ props.email }}
         </v-card-subtitle>
     </v-card>
 </template>
 <script setup lang="ts">
-import { defineProps } from "vue";
 
-interface Props {
-    email: string;
-}
 
-const props = withDefaults(defineProps<Props>(), {
-    email: "",
-});
 </script>
 <style scoped></style>
