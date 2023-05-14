3b846bf2fb26bce4fb247610106c063c98d53fe5
metal-face <hughesbryan3000@gmail.com>
2023-05-13 23:45:56 -0400

    Component controllable from parent with prop support

--- client/src/components/BaseInput.vue
+++ client/src/components/BaseInput.vue
@@ -1,24 +1,30 @@
 <template>
-    <v-card flat>
-        <v-text-field
-            v-model="input"
-            :label="props.label"
-            :required="props.required"
-            @input="onInput" />
-    </v-card>
+    <v-text-field
+        v-model="input"
+        :label="props.label"
+        :variant="props.variant"
+        :counter="props.counter"
+        :placeholder="props.placeholder"
+        density="comfortable"
+        @input="onInput" />
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 
 interface Props {
-    label?: string;
-    required: boolean;
+    email?: string;
+    label: string;
+    variant: string;
+    counter: number;
+    placeholder: string;
 }
 
 const props = withDefaults(defineProps<Props>(), {
+    email: "",
     label: "",
-    required: false,
+    variant: "plain",
+    placeholder: "",
 });
 
 const input = ref<string>("");
@@ -30,6 +36,12 @@ const emit = defineEmits<{
 function onInput() {
     emit("input", input.value);
 }
+
+onMounted(() => {
+    if (props.email !== "") {
+        input.value = props.email;
+    }
+});
 </script>
 
 <style scoped></style>
