2469a8159e67bbcef4a1ae8aaa1a942176d4e37c
metal-face <hughesbryan3000@gmail.com>
2023-05-19 03:06:07 -0400

    Improvements/Fixes
    Form placed into Vuetify grid system.
    
    Form title font styled.
    
    Username and password fields added.
    
    Password field provided visibility toggle.

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -2,10 +2,36 @@
     <v-row justify="center">
         <v-col cols="6">
             <v-card height="100%" flat>
-                <v-card-title class="text-center"> Register </v-card-title>
+                <v-card-title class="text-center">
+                    <h2 class="form-title ma-3">Register</h2>
+                </v-card-title>
                 <v-form>
-                    <v-text-field v-if="!email" :value="email" disabled />
-                    <v-text-field v-else v-model="email" />
+                    <v-container fluid>
+                        <v-row dense>
+                            <v-col cols="12">
+                                <v-text-field
+                                    v-model="email"
+                                    variant="outlined"
+                                    label="Email"
+                                    :disabled="email ? true : false" />
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field
+                                    v-model="username"
+                                    label="Username"
+                                    variant="outlined" />
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field
+                                    v-model="password"
+                                    @click:append-inner="toggleVisibility"
+                                    append-inner-icon="mdi-eye"
+                                    :type="visible ? 'text' : 'password'"
+                                    variant="outlined"
+                                    label="Password" />
+                            </v-col>
+                        </v-row>
+                    </v-container>
                 </v-form>
             </v-card>
         </v-col>
@@ -23,5 +49,16 @@ onMounted(() => {
 });
 
 const email = ref<string>("");
+const password = ref<string>("");
+const username = ref<string>("");
+const visible = ref<boolean>();
+
+function toggleVisibility() {
+    visible.value = !visible.value;
+}
 </script>
-<style scoped></style>
+<style scoped>
+.form-title {
+    font-family: "Playfair Display", serif;
+}
+</style>
