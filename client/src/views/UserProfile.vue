9bfb2d6f2eb9e166cb3fbbc7b3feb8b13ff7c84c
metal-face <hughesbryan3000@gmail.com>
2023-05-23 02:10:06 -0400

    user profile started

---
+++ client/src/views/UserProfile.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-row dense>
+        <v-col cols="12">
+            <v-card>
+                <v-card-title>
+                    {{ username }}
+                </v-card-title>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+
+<script setup lang="ts">
+import { useAppStore } from "@/store/app";
+import { onMounted, ref } from "vue";
+
+const store = useAppStore();
+
+const username = ref<string>("");
+
+onMounted(() => {
+    username.value = store.getUsername;
+});
+</script>
+
+<style scoped></style>
