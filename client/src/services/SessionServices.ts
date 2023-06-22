c7d358c2d6b7f3f4ba6146a19282b1c15a997682
metal-face <hughesbryan3000@gmail.com>
2023-06-22 01:38:40 -0400

    sessions service layer written

---
+++ client/src/services/SessionServices.ts
@@ -0,0 +1,39 @@
+import ApiClient from "@/clients/ApiClient"
+import { AxiosResponse } from "axios";
+
+export default {
+    login(username: string, password: string): Promise<AxiosResponse<any, any>> {
+        return ApiClient({
+            method: "POST",
+            url: "/sessions",
+            data: {
+                username: username,
+                password: password,
+            }
+        })
+    },
+    fetchManySessions(accountId?: string, user_agent?: string, page?: number, pageSize?: number) {
+        return ApiClient({
+            method: "GET",
+            url: `/sessions?account_id=${accountId}&page=${page}&page_size=${pageSize}`,
+            headers: {
+                "User-Agent": user_agent
+            }
+        })
+    },
+    fetchSessionById(session_id: string): Promise<AxiosResponse<any, any>> {
+        return ApiClient({
+            method: "GET",
+            url: `/ssessions/${session_id}`
+        })
+    },
+    logout(sessionId: string): Promise<AxiosResponse<any, any>> {
+        return ApiClient({
+            method: "DELETE",
+            url: `/sessions`,
+            data: {
+                session_id: sessionId
+            }
+        })
+    }
+}
