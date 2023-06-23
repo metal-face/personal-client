93503692bebbd4d2bf678985ddae573830283cd6
metal-face <hughesbryan3000@gmail.com>
2023-06-22 22:29:30 -0400

    custom color themes added

--- client/src/plugins/vuetify.ts
+++ client/src/plugins/vuetify.ts
@@ -10,8 +10,37 @@ import "vuetify/styles";
 import { md3 } from "vuetify/blueprints";
 
 // Composables
-import { createVuetify } from "vuetify";
+import { createVuetify, ThemeDefinition } from "vuetify";
 import { aliases, mdi } from "vuetify/iconsets/mdi";
+import colors from "vuetify/lib/util/colors";
+
+const customDarkTheme: ThemeDefinition = {
+    dark: true,
+    colors: {
+        primary: colors.grey.darken4,
+        secondary: colors.pink.darken1,
+        background: colors.shades.black,
+        surface: colors.grey.darken3,
+        error: colors.red.accent1,
+        info: colors.blue.accent1,
+        success: colors.green.accent2,
+        warning: colors.yellow.accent1,
+    }
+}
+
+const customLightTheme: ThemeDefinition = {
+    dark: false,
+    colors: {
+        primary: colors.shades.black,
+        secondary: colors.pink.lighten1,
+        background: colors.shades.white,
+        surface: colors.grey.lighten5,
+        error: colors.red.accent1,
+        info: colors.blue.accent1,
+        success: colors.green.accent2,
+        warning: colors.yellow.accent1,
+    }
+}
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
     blueprint: md3,
@@ -23,20 +52,10 @@ export default createVuetify({
         },
     },
     theme: {
-        defaultTheme: "dark",
+        defaultTheme: 'customDarkTheme',
         themes: {
-            light: {
-                colors: {
-                    primary: "#1867C0",
-                    secondary: "#5CBBF6",
-                },
-            },
-            dark: {
-                colors: {
-                    primary: "#1e1e1e",
-                    secondary: "#4e4e4e",
-                },
-            },
+            customDarkTheme,
+            customLightTheme,
         },
     },
 });
