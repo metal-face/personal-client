280aa7d791f8ce27c18b2b77de222ddaf0b07c61
metal-face <hughesbryan3000@gmail.com>
2023-06-29 22:58:25 -0400

    session properties made non-optional

--- client/src/models/Session.ts
+++ client/src/models/Session.ts
@@ -1,7 +1,7 @@
 export interface Session {
-    account_id?: string;
-    session_id?: string;
-    created_at?: string;
-    expires_at?: string;
-    user_agent?: string;
+    account_id: string;
+    session_id: string;
+    created_at: Date;
+    expires_at: Date;
+    user_agent: string;
 }
