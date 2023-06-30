869993a8b211f683d0a12adbb3647df21c8ec4b2
metal-face <hughesbryan3000@gmail.com>
2023-06-29 22:58:56 -0400

    created/expires_at changed to Date values, more getters added

--- client/src/store/SessionStore.ts
+++ client/src/store/SessionStore.ts
@@ -3,9 +3,8 @@ import { defineStore } from "pinia";
 import { Session } from "@/models/Session"
 
 interface State {
-    session: Session | {};
+    session: Session;
 }
-
 
 export const sessionStore = defineStore("session", {
     state: (): State => {
@@ -13,21 +12,39 @@ export const sessionStore = defineStore("session", {
             session: {
                 account_id: "",
                 session_id: "",
-                created_at: "",
-                expires_at: "",
+                created_at: new Date(),
+                expires_at: new Date(),
                 user_agent: "",
             },
         }
     },
     actions: {
-        setSession(session: Session) {
+        setSession(session: Session): void {
             window.localStorage.setItem("session", JSON.stringify(session));
             Object.assign(this.session, session);
-        }
+        },
+        destroySession(): void {
+            window.localStorage.removeItem("session");
+        },
     },
     getters: {
-        getSession(state: State) {
+        getSession(state: State): Session {
             return state.session;
+        },
+        isLoggedIn(state: State): boolean {
+            return state.session.account_id !== "";
+        },
+        isExpired(state: State): boolean {
+           return state.session.expires_at < new Date();
+        },
+        createdAt(state: State): Date {
+            return state.session.created_at;
+        },
+        expiresAt(state: State): Date {
+            return state.session.expires_at;
+        },
+        accountID(state: State): string {
+            return state.session.account_id;
         },
     }
 });
