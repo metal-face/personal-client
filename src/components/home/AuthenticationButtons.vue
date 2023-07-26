e64c8e889df6cca9fbe668b1744b70fa5e86ce0c
metal-face <hughesbryan3000@gmail.com>
2023-07-25 23:44:12 -0400

    Auth buttons encapsulated into their own buttons

---
+++ src/components/home/AuthenticationButtons.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="d-flex align-center justify-space-between">
+        <v-btn
+            @click="router.push({ name: 'Login' })"
+            size="x-large"
+            elevation="12"
+            width="49%"
+            rounded="2">
+            Login
+        </v-btn>
+        <v-btn
+            @click="router.push({ name: 'Register' })"
+            width="49%"
+            elevation="12"
+            size="x-large"
+            rounded="2">
+            Register
+        </v-btn>
+    </div>
+</template>
+<script setup lang="ts">
+import { useRouter, Router } from "vue-router";
+
+const router: Router = useRouter();
+</script>
