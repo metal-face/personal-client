241d0f6a1a77a3c68a72aa8fc3c14d58d13b4fc7
metal-face <hughesbryan3000@gmail.com>
2023-07-25 23:46:29 -0400

    Login with github button encapsulated

---
+++ src/components/home/GitHubLoginButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <v-btn
+            size="x-large"
+            block
+            rounded="2"
+            color="secondary"
+            elevation="12"
+            append-icon="mdi-github">
+            Login with GitHub
+        </v-btn>
+    </div>
+</template>
+<script setup lang="ts"></script>
