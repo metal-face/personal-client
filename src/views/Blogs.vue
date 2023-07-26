115350b0d3c8c4157c5375a8509a13252cf7cd7f
metal-face <hughesbryan3000@gmail.com>
2023-07-26 01:32:31 -0400

    blog component started

---
+++ src/views/Blogs.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-row class="fill-height">
+        <v-col>
+            <v-card class="fill-height">
+                <v-card-title>
+                    Blogs
+                </v-card-title>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+<script setup lang="ts">
+
+</script>
