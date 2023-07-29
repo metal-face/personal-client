2a053cc3c392a59f5ce5fb69b93cb1fd523b422e
metal-face <hughesbryan3000@gmail.com>
2023-07-28 22:55:00 -0400

    Blogs view started

--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -2,8 +2,11 @@
     <v-row class="fill-height">
         <v-col>
             <v-card class="fill-height">
+                <!-- TODO: list all blogs for the signed in user -->
+                <!-- TODO: if there are no blogs, diplay an indicator to create a blog. -->
+                <!-- TODO: add a create button that redirects the user to the create page. -->
                 <v-card-title>
-                    Blogs
+                   Your Blogs
                 </v-card-title>
             </v-card>
         </v-col>
