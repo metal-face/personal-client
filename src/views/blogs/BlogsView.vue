28c36a733493e6e629f5b64d303ef506778cd946
metal-face <hughesbryan3000@gmail.com>
2023-07-31 01:47:27 -0400

    Top level view emptied.
    Child routes taken advantage of and content that was here has been
    moved to child views, this view now just contains the router view
    component.
    
    In the future, search or a title may be added here.

---
+++ src/views/blogs/BlogsView.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-row justify="center" class="fill-height">
+        <v-col cols="12">
+            <router-view />
+        </v-col>
+    </v-row>
+</template>
+<script setup lang="ts"></script>
+<style scoped>
+.page-title {
+    font-family: "Prata", "serif" ;
+}
+</style>
