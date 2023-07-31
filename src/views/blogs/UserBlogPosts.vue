0603bca0b328c8139a60f0919a4c449637e3a7e6
metal-face <hughesbryan3000@gmail.com>
2023-07-31 01:46:16 -0400

    View to hold user's blogs created.
    A create button was added to the template. When clicked, the user
    is redirected to the blog creator page.

---
+++ src/views/blogs/UserBlogPosts.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-card variant="elevated" color="primary" rounded="1" class="fill-height">
+        <!-- TODO: list all blogs for the signed in user -->
+        <!-- TODO: if there are no blogs, diplay an indicator to create a blog. -->
+        <!-- TODO: add a create button that redirects the user to the create page. -->
+        <v-card variant="flat" color="primary" rounded="1">
+            <v-card variant="flat" color="primary" rounded="0">
+                <v-card-actions class="my-0">
+                    <v-btn
+                        position="absolute"
+                        location="right"
+                        variant="elevated"
+                        :to="{ name: 'BlogCreator' }"
+                        class="my-1 mr-2"
+                        color="accent"
+                        rounded="1"
+                        append-icon="mdi-pencil">
+                        create
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+            <v-card-title class="page-title text-center ma-3">
+                <h1 class="text-decoration-underline">Blog Posts</h1>
+            </v-card-title>
+        </v-card>
+    </v-card>
+</template>
+<script lang="ts">
+</script>
+<style scoped>
+.page-title {
+    font-family: "Prata", "serif" ;
+}
+</style>
