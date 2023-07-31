34b3e6c2e1ceab47fb10f6e091bc5816273e309e
metal-face <hughesbryan3000@gmail.com>
2023-07-31 01:35:28 -0400

    Blog post card component created

---
+++ src/components/blogs/BlogPostCard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+    blogTitle: string;
+    creationDate: Date;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+    <v-card>
+        <v-card-title>{{ props.blogTitle }}</v-card-title>
+        <v-card-subtitle>{{ props.creationDate }}</v-card-subtitle>
+    </v-card>
+</template>
+
+<style scoped></style>
