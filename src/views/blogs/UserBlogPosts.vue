a7102c1a762222ab37ebc126da9fdaaef65c5729
metal-face <hughesbryan3000@gmail.com>
2023-08-01 00:12:08 -0400

    Create button made to look more familiar to users.

--- src/views/blogs/UserBlogPosts.vue
+++ src/views/blogs/UserBlogPosts.vue
@@ -1,34 +1,38 @@
+<script setup lang="ts">
+import { ThemeInstance, useTheme } from "vuetify";
+import { computed } from "vue";
+
+const theme: ThemeInstance = useTheme();
+
+let isDark = computed<boolean>(() => {
+    return theme.global.current.value.dark;
+});
+</script>
+<style scoped>
+.page-title {
+    font-family: "Prata", "serif";
+}
+</style>
 <template>
-    <v-card variant="elevated" color="primary" rounded="1" class="fill-height">
+    <v-card elevation="12" variant="elevated" color="primary" rounded="1" class="fill-height">
         <!-- TODO: list all blogs for the signed in user -->
         <!-- TODO: if there are no blogs, diplay an indicator to create a blog. -->
         <!-- TODO: add a create button that redirects the user to the create page. -->
         <v-card variant="flat" color="primary" rounded="1">
-            <v-card variant="flat" color="primary" rounded="0">
-                <v-card-actions class="my-0">
-                    <v-btn
-                        position="absolute"
-                        location="right"
-                        variant="elevated"
-                        :to="{ name: 'BlogCreator' }"
-                        class="my-1 mr-2"
-                        color="accent"
-                        rounded="1"
-                        append-icon="mdi-pencil">
-                        create
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
             <v-card-title class="page-title text-center ma-3">
                 <h1 class="text-decoration-underline">Blog Posts</h1>
             </v-card-title>
         </v-card>
+        <v-btn
+            :to="{ name: 'BlogCreator' }"
+            :color="isDark ? 'white' : 'black'"
+            position="absolute"
+            location="bottom right"
+            variant="elevated"
+            size="x-large"
+            rounded="3"
+            class="ma-3"
+            icon="mdi-pencil">
+        </v-btn>
     </v-card>
 </template>
-<script lang="ts">
-</script>
-<style scoped>
-.page-title {
-    font-family: "Prata", "serif" ;
-}
-</style>
