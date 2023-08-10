3d8fb4acbe376923693f228e76b40be01cbd4d84
metal-face <hughesbryan3000@gmail.com>
2023-08-10 00:52:06 -0400

    Theme and display helpers imported into component.

--- src/views/blogs/BlogsView.vue
+++ src/views/blogs/BlogsView.vue
@@ -5,9 +5,23 @@
         </v-col>
     </v-row>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDisplay, useTheme, ThemeInstance, DisplayInstance } from "vuetify";
+import { computed, onMounted } from "vue";
+
+const display: DisplayInstance = useDisplay();
+const theme: ThemeInstance = useTheme();
+
+onMounted(() => {
+    console.log(display);
+});
+
+const isMobile = computed<boolean>(() => {
+    return display.mobile.value;
+});
+</script>
 <style scoped>
 .page-title {
-    font-family: "Prata", "serif" ;
+    font-family: "Prata", "serif";
 }
 </style>
