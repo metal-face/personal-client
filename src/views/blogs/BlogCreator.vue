de4451412f5ee123ed128cd7429911df5b15b1d1
metal-face <hughesbryan3000@gmail.com>
2023-08-12 02:35:02 -0400

    All editor dependencies imported and referenced.

--- src/views/blogs/BlogCreator.vue
+++ src/views/blogs/BlogCreator.vue
@@ -9,46 +9,37 @@
         </v-card-title>
 
         <v-row dense no-gutters>
-            <v-col cols="2">
-                <v-card variant="flat" color="transparent" class="mr-1">
-                    <v-select
-                        v-model="codeThemePreference"
-                        :items="codeTheme"
-                        variant="solo"
-                        density="compact"
-                        item-title="text"
-                        item-value="item-value"
-                        label="Code Theme Preference" />
-                </v-card>
-            </v-col>
-            <v-col cols="2">
-                <v-card variant="flat" color="transparent" class="ml-auto">
-                    <v-select
-                        v-model="previewThemePreference"
-                        :items="previewTheme"
-                        :menu-props="{
-                            offset: 5,
-                        }"
-                        item-title="text"
-                        item-value="value"
-                        density="compact"
-                        label="Select preview theme"
-                        variant="solo" />
-                </v-card>
-            </v-col>
+            <v-row dense no-gutters justify="center" align-content="center">
+                <v-col cols="3">
+                    <v-card variant="flat" color="transparent" class="ma-1">
+                        <v-select
+                            v-model="previewThemePreference"
+                            :items="previewTheme"
+                            :menu-props="{
+                                offset: 5,
+                            }"
+                            item-title="text"
+                            item-value="value"
+                            density="compact"
+                            label="Select preview theme"
+                            variant="solo" />
+                    </v-card>
+                </v-col>
+            </v-row>
             <v-col cols="12">
                 <MdEditor
                     v-model="userInput"
                     language="en-US"
+                    class="page-title"
                     :theme="isDark ? 'dark' : 'light'"
                     :sanitize="sanitize"
                     :preview-theme="previewThemePreference"
-                    :code-theme="codeThemePreference"
-                    :page-fullscreen="fullscreen"
-                    :preview="preview"
                     :tab-width="4"
+                    :completions="completions"
                     :table-shape="tableShape"
+                    code-theme="github"
                     auto-detect-code
+                    no-prettier
                     show-code-row-number />
             </v-col>
         </v-row>
@@ -82,65 +73,81 @@ import { useRouter, Router } from "vue-router";
 import { ref, computed, reactive } from "vue";
 import { ThemeInstance, useTheme } from "vuetify";
 import { Session } from "@/models/Session";
+import { lineNumbers, crosshairCursor } from "@codemirror/view";
 import { MdEditor, config } from "md-editor-v3";
+import { CompletionSource } from "@codemirror/autocomplete";
 import sanitizeHtml from "sanitize-html";
 import BlogServices from "@/services/BlogServices";
+import screenfull from "screenfull";
+import katex from "katex";
+import mermaid from "mermaid";
+import Cropper from "cropperjs";
+import highlight from "highlight.js";
+
+import "highlight.js/styles/github.css";
+import "cropperjs/dist/cropper.css";
 import "md-editor-v3/lib/style.css";
-import { lineNumbers } from "@codemirror/view";
+import "katex/dist/katex.min.css";
 
 interface PreviewTheme {
     text: string;
     value: string;
 }
 
-interface CodeTheme {
-    text: String;
-    value: String;
-}
-
 const theme: ThemeInstance = useTheme();
 const router: Router = useRouter();
 
-config({
-    codeMirrorExtensions(_theme, extensions) {
-        return [...extensions, lineNumbers()];
-    },
-    editorConfig: {
-        // mermaid template
-        mermaidTemplate: {
-            flow: `flow`,
-            sequence: `sequence`,
-            gantt: `gantt`,
-            class: `class`,
-            state: `state`,
-            pie: `pie`,
-            relationship: `relationship`,
-            journey: `journey`,
-        },
-    },
+const completions = ref<Array<CompletionSource>>([
+    (context) => {
+        const word = context.matchBefore(/@\w*/);
 
+        if (word === null || (word.from == word.to && context.explicit)) {
+            return null;
+        }
+
+        return {
+            from: word.from,
+            options: [
+                {
+                    label: "@imzbf",
+                    type: "text",
+                },
+            ],
+        };
+    },
+]);
+
+config({
     editorExtensions: {
-        highlight: {
-            css: {
-                atom: {
-                    light: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/styles/atom-one-light.min.css",
-                    dark: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/styles/atom-one-dark.min.css",
-                },
-                xxx: {
-                    light: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/styles/xxx-light.css",
-                    dark: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/styles/xxx-dark.css",
-                },
-            },
+        prettier: {
+            // prettierInstance: prettier,
+            // parserMarkdownInstance: parserMarkdown,
         },
+        highlight: {
+            instance: highlight,
+        },
+        screenfull: {
+            instance: screenfull,
+        },
+        katex: {
+            instance: katex,
+        },
+        cropper: {
+            instance: Cropper,
+        },
+        mermaid: {
+            instance: mermaid,
+        },
+    },
+    codeMirrorExtensions(_theme, extensions) {
+        return [...extensions, lineNumbers(), crosshairCursor()];
     },
 });
 
-const sanitize: any = (html: string) => sanitizeHtml(html);
-const userInput = ref("# You can write markdown here!");
-const preview = ref<boolean>(true);
+const sanitize = (html: string): string => sanitizeHtml(html);
+const userInput = ref("# Start your post here!");
 const tableShape = reactive<number[]>([8, 4]);
 const previewThemePreference = ref<string>("github");
-const codeThemePreference = ref<string>("github");
 
 const previewTheme = reactive<PreviewTheme[]>([
     { text: "Default", value: "default" },
@@ -149,16 +156,6 @@ const previewTheme = reactive<PreviewTheme[]>([
     { text: "MKCute", value: "mk-cute" },
     { text: "Smart Blue", value: "smart-blue" },
     { text: "Cyanosis", value: "cyanosis" },
-]);
-const codeTheme = reactive<CodeTheme[]>([
-    { text: "Atom", value: "atom" },
-    { text: "a11y", value: "a11y" },
-    { text: "GitHub", value: "github" },
-    { text: "Gradient", value: "gradient" },
-    { text: "Kimbie", value: "kimbie" },
-    { text: "Paraiso", value: "paraiso" },
-    { text: "QTCreator", value: "qtcreator" },
-    { text: "StackOverflow", value: "stackoverflow" },
 ]);
 
 const sessionFromStorage: string = window.localStorage.getItem("session")!;
@@ -183,12 +180,11 @@ async function createBlogPost() {
         });
 }
 </script>
-<style scoped>
+<style>
+.default-theme h1 {
+    margin: 0 !important;
+}
 .page-title {
     font-family: "Prata", "serif";
-}
-
-.markdown-output {
-    height: 100%;
 }
 </style>
