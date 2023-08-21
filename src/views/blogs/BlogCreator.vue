49925742cade6ebc94b2d6dc44fb5687c146dbde
metal-face <hughesbryan3000@gmail.com>
2023-08-20 23:28:52 -0400

    imageUpload and saveContent methods written.

--- src/views/blogs/BlogCreator.vue
+++ src/views/blogs/BlogCreator.vue
@@ -1,29 +1,31 @@
 <template>
-    <v-row>
-        <v-col cols="12">
+    <v-row class="fill-height" justify="center" align="center" dense no-gutters>
+        <v-col :cols="editorWidth">
             <v-card
                 :elevation="isDark ? 0 : 12"
                 :variant="isDark ? 'flat' : 'elevated'"
-                class="fill-height d-flex flex-column"
+                class="fill-height"
                 color="background">
-                <v-row dense justify="center" align-content="center">
-                    <v-col v-if="!titleState" cols="12">
+                <v-row justify="center" align-content="center">
+                    <!-- Actual Title -->
+                    <v-col v-if="!titleState" :cols="12">
                         <v-card @click="titleState = true" flat color="transparent">
                             <v-card-title class="d-flex justify-center align-center ma-3">
                                 <h1 class="page-title">{{ blogPostTitle }}</h1>
                             </v-card-title>
                         </v-card>
                     </v-col>
-                    <v-col v-if="titleState" cols="3">
+                    <!-- Title Editor -->
+                    <v-col v-if="titleState" :cols="inputWidth">
                         <v-card flat color="transparent" width="100%" class="mx-auto">
                             <v-card-title>
                                 <v-text-field
                                     v-model="blogPostTitle"
                                     @keydown="handleKeyDown"
                                     autofocus
+                                    hint="You can press enter to save!"
                                     class="interactive-title"
-                                    variant="solo"
-                                    hide-details>
+                                    variant="solo">
                                     <template #append-inner>
                                         <v-icon>mdi-keyboard-return</v-icon>
                                     </template>
@@ -71,20 +73,21 @@
                     <!--                        </v-card>-->
                     <!--                    </v-col>-->
 
-                    <v-col cols="12">
+                    <v-col :cols="12">
                         <MdEditor
+                            @onSave="saveContentToLocalStorage"
+                            @onUploadImage="handleImageUpload"
                             v-model="blogPostBody"
-                            @onChange="sanitizeCode"
-                            ref="editorRef"
                             :theme="isDark ? 'dark' : 'light'"
                             :preview-theme="previewThemePreference"
                             :tab-width="4"
                             :table-shape="tableShape"
                             :code-theme="codeThemePreference"
+                            ref="editorRef"
+                            language="en-US"
                             auto-detect-code
                             no-prettier
                             show-code-row-number
-                            language="en-US"
                             class="page-title" />
                     </v-col>
                 </v-row>
@@ -118,9 +121,8 @@
 </template>
 <script setup lang="ts">
 import { useRouter, Router } from "vue-router";
-import { ref, computed, reactive, watch, nextTick } from "vue";
+import { ref, computed, reactive, watch, onMounted } from "vue";
 import { ThemeInstance, useTheme, useDisplay, DisplayInstance } from "vuetify";
-import { Session } from "@/models/Session";
 import { lineNumbers } from "@codemirror/view";
 import { MdEditor, config } from "md-editor-v3";
 import sanitizeHtml from "sanitize-html";
@@ -131,6 +133,7 @@ import mermaid from "mermaid";
 import Cropper from "cropperjs";
 import highlight from "highlight.js";
 import type { ExposeParam } from "md-editor-v3";
+import { sessionStore } from "@/store/SessionStore";
 
 import "highlight.js/styles/github.css";
 import "cropperjs/dist/cropper.css";
@@ -147,23 +150,90 @@ interface CodeTheme {
     value: String;
 }
 
-let mermaidConfig = {
-    startOnLoad: true,
-    securityLevel: "strict",
-    flowchart: { titleTopMargin: 1 },
-    gantt: { titleTopMargin: 25, barHeight: 20, topPadding: 50 },
-};
-
-function sanitizeCode(code: string): void {
-    // blogPostBody.value = sanitize(code);
-}
-
-mermaid.initialize(mermaidConfig);
+const sanitize = (html: string): string => sanitizeHtml(html);
 
 const theme: ThemeInstance = useTheme();
 const router: Router = useRouter();
 const display: DisplayInstance = useDisplay();
+const sessStore = sessionStore();
 
+const blogPostBody = ref("# Write your post here.");
+const blogPostTitle = ref<string>("A Good Blog Post Title");
+
+const tableShape = reactive<number[]>([8, 4]);
+const previewThemePreference = ref<string>("default");
+const codeThemePreference = ref<string>("default");
+const titleState = ref<boolean>(false);
+const editorRef = ref<ExposeParam>();
+const mermaidConfig = {
+    startOnLoad: true,
+    securityLevel: "strict",
+};
+
+const previewTheme = reactive<PreviewTheme[]>([
+    { text: "Default", value: "default" },
+    { text: "GitHub", value: "github" },
+    { text: "MKCute", value: "mk-cute" },
+    { text: "Cyanosis", value: "cyanosis" },
+]);
+const codeTheme = reactive<CodeTheme[]>([
+    { text: "Atom", value: "atom" },
+    { text: "a11y", value: "a11y" },
+    { text: "GitHub", value: "github" },
+    { text: "Gradient", value: "gradient" },
+    { text: "Kimbie", value: "kimbie" },
+    { text: "Paraiso", value: "paraiso" },
+    { text: "QTCreator", value: "qtcreator" },
+    { text: "StackOverflow", value: "stackoverflow" },
+]);
+
+const inputWidth = computed<number>(() => {
+    switch (display.name.value) {
+        case "xs":
+            return 10;
+        case "sm":
+            return 8;
+        case "md":
+            return 6;
+        case "lg":
+            return 4;
+        case "xl":
+            return 4;
+        case "xxl":
+            return 4;
+        default:
+            return 6;
+    }
+});
+
+const editorWidth = computed<number>(() => {
+    switch (display.name.value) {
+        case "xs":
+            return 12;
+        case "sm":
+            return 11;
+        case "md":
+            return 10;
+        case "lg":
+            return 10;
+        default:
+            return 10;
+    }
+});
+
+const darkState = computed<string>(() => {
+    return theme.global.name.value;
+});
+
+const isDark = computed<boolean>(() => {
+    return darkState.value === "customDarkTheme";
+});
+
+const accountId = computed<string>(() => {
+    return sessStore.accountID;
+});
+
+// MDEditor configuration.
 config({
     editorExtensions: {
         highlight: {
@@ -186,51 +256,13 @@ config({
         return [...extensions, lineNumbers()];
     },
 });
+// Mermaid Initialization
+mermaid.initialize(mermaidConfig);
 
-const sanitize = (html: string): string => sanitizeHtml(html);
-const blogPostBody = ref("# Write your post here.");
-const blogPostTitle = ref<string>("A Good Blog Post Title");
-const tableShape = reactive<number[]>([8, 4]);
-const previewThemePreference = ref<string>("default");
-const codeThemePreference = ref<string>("default");
-const previewState = ref<boolean>(true);
-const titleState = ref<boolean>(false);
-const editorRef = ref<ExposeParam>();
-
-editorRef.value?.on("preview", console.log);
-
-const previewTheme = reactive<PreviewTheme[]>([
-    { text: "Default", value: "default" },
-    { text: "GitHub", value: "github" },
-    { text: "MKCute", value: "mk-cute" },
-    { text: "Cyanosis", value: "cyanosis" },
-]);
-const codeTheme = reactive<CodeTheme[]>([
-    { text: "Atom", value: "atom" },
-    { text: "a11y", value: "a11y" },
-    { text: "GitHub", value: "github" },
-    { text: "Gradient", value: "gradient" },
-    { text: "Kimbie", value: "kimbie" },
-    { text: "Paraiso", value: "paraiso" },
-    { text: "QTCreator", value: "qtcreator" },
-    { text: "StackOverflow", value: "stackoverflow" },
-]);
-
-const sessionFromStorage: string = window.localStorage.getItem("session")!;
-const session: Session = JSON.parse(sessionFromStorage);
-
-const darkState = computed<string>(() => {
-    return theme.global.name.value;
-});
-const isDark = computed<boolean>(() => {
-    return darkState.value === "customDarkTheme";
-});
-
+// Display breakpoint watcher
 watch(
     display.name,
-    async (newVal) => {
-        await nextTick();
-
+    (newVal) => {
         switch (newVal) {
             case "xs":
                 editorRef.value?.togglePreview(false);
@@ -258,21 +290,69 @@ watch(
     { immediate: true },
 );
 
+/**
+ * Sanitizes the user input to protect
+ * @param code
+ */
+function sanitizeCode(code: string): void {
+    blogPostBody.value = sanitize(code);
+}
+
+/**
+ * Saves content in editor to local storage
+ * @param saveContent
+ */
+const saveContentToLocalStorage = (saveContent: string) => {
+    window.localStorage.setItem("blog_content", saveContent);
+    window.localStorage.setItem("blog_title", blogPostTitle.value);
+};
+
+const handleImageUpload = (files, callback) => {
+    console.log(files);
+};
+
+/**
+ * Handles the keydown event, specifically
+ * the enter event. When this happens, the
+ * text-field to edit the blog post title is
+ * hidden.
+ * @param {KeyboardEvent} e
+ */
 function handleKeyDown(e: KeyboardEvent): void {
     if (e.code === "Enter") {
         titleState.value = false;
     }
 }
 
+/**
+ * Makes a request to the server to create
+ * a blog post using the service layer.
+ *
+ * Sanitizes the data before sending it to the server.
+ */
 async function createBlogPost() {
-    BlogServices.createBlogPost(blogPostTitle.value, blogPostBody.value, session.account_id)
-        .then((res) => {
+    sanitizeCode(blogPostBody.value);
+    BlogServices.createBlogPost(blogPostTitle.value, blogPostBody.value, accountId.value)
+        .then(() => {
             router.push({ name: "UserBlogPosts" });
         })
         .catch((err) => {
             console.error(err);
         });
 }
+
+onMounted(() => {
+    const savedContent: string | null = window.localStorage.getItem("blog_content");
+    const savedTitle: string | null = window.localStorage.getItem("blog_title");
+
+    if (savedContent) {
+        blogPostBody.value = savedContent;
+    }
+
+    if (savedTitle) {
+        blogPostTitle.value = savedTitle;
+    }
+});
 </script>
 <style>
 .interactive-title {
