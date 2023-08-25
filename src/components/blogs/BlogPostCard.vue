c500c8d7e95b9bb189a75914e4594c7eb1ac19c4
metal-face <hughesbryan3000@gmail.com>
2023-08-25 01:55:27 -0400

    Individual blog cards with title and date added.

--- src/components/blogs/BlogPostCard.vue
+++ src/components/blogs/BlogPostCard.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 interface Props {
     blogTitle: string;
-    creationDate: Date;
+    blogPost: string;
+    creationDate: string;
 }
 
 const props = defineProps<Props>();
 </script>
 
 <template>
-    <v-card>
+    <v-card variant="flat" color="transparent" width="100%">
         <v-card-title>{{ props.blogTitle }}</v-card-title>
         <v-card-subtitle>{{ props.creationDate }}</v-card-subtitle>
     </v-card>
