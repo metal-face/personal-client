216596c947053e1f2443f10b9b0e6a7ebaaa72e0
metal-face <hughesbryan3000@gmail.com>
2023-09-01 00:28:55 -0400

    Ability to update blog added.

--- src/views/blogs/BlogUpdater.vue
+++ src/views/blogs/BlogUpdater.vue
@@ -1,5 +1,137 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MarkdownEditor from "@/components/blogs/MarkdownEditor.vue";
+import { computed, onMounted, reactive, ref } from "vue";
+import { useTheme, ThemeInstance } from "vuetify";
+import { RouteParamsRaw, Router, useRouter } from "vue-router";
+import BlogServices from "@/services/BlogServices";
+import { Snackbar } from "@/models/Snackbar";
+import { sessionStore } from "@/store/SessionStore";
 
-<template></template>
+interface Props {
+    blogId: string;
+    readonly: string;
+}
+
+const props = defineProps<Props>();
+const router: Router = useRouter();
+const theme: ThemeInstance = useTheme();
+const sessStore = sessionStore();
+
+const snackbar = reactive<Snackbar>({
+    visible: false,
+    color: "success",
+    timeout: 3000,
+    text: "",
+});
+
+const blogPostTitle = ref<string>("");
+const blogPostBody = ref<string>("");
+
+const darkState = computed<string>(() => {
+    return theme.global.name.value;
+});
+const isDark = computed<boolean>(() => {
+    return darkState.value === "customDarkTheme";
+});
+
+const accountId = computed<string>(() => {
+    return sessStore.accountID;
+});
+
+function redirectUser(name: string, params?: RouteParamsRaw): void {
+    router.push({ name: name, params: params });
+}
+
+function fetchBlogPostById(): void {
+    BlogServices.fetchBlogById(props.blogId).then((res) => {
+        console.log(res);
+        if (res.data.data.blog_post) {
+            blogPostBody.value = res.data.data.blog_post;
+        }
+        if (res.data.data.blog_title) {
+            blogPostTitle.value = res.data.data.blog_title;
+        }
+    });
+}
+
+function handleTitleChange(change: string): void {
+    blogPostTitle.value = change;
+}
+function handleBodyChange(change: string): void {
+    blogPostBody.value = change;
+}
+
+function showSnackbar(): void {
+    snackbar.visible = true;
+    snackbar.color = "success";
+    snackbar.text = "Saved to your browser! 💾";
+}
+
+function updateBlogById() {
+    BlogServices.updateBlogById(
+        props.blogId,
+        accountId.value,
+        blogPostTitle.value,
+        blogPostBody.value,
+    )
+        .then(() => {
+            router.push({ name: "BlogPosts" });
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+}
+
+onMounted(() => {
+    if (props.blogId) {
+        fetchBlogPostById();
+    }
+});
+</script>
+
+<template>
+    <v-row>
+        <v-snackbar
+            v-model="snackbar.visible"
+            :timeout="snackbar.timeout"
+            :color="snackbar.color"
+            location="center top"
+            variant="elevated">
+            <h3 class="text-center">{{ snackbar.text }}</h3>
+        </v-snackbar>
+        <v-col cols="12">
+            <v-card flat variant="elevated" elevation="12" :color="isDark ? 'black' : 'white'">
+                <!--TODO: add event listener for body and title changes, save them locally in this component-->
+                <MarkdownEditor
+                    @title:change="handleTitleChange"
+                    @body:change="handleBodyChange"
+                    @save:local="showSnackbar"
+                    :readonly="props.readonly"
+                    :blogId="props.blogId" />
+                <v-card-actions class="d-flex flex-column mx-auto">
+                    <v-btn
+                        @click="updateBlogById"
+                        variant="elevated"
+                        color="accent"
+                        block
+                        size="x-large"
+                        rounded="1"
+                        class="mx-auto">
+                        Save
+                    </v-btn>
+                    <v-btn
+                        @click="redirectUser('BlogPosts')"
+                        variant="elevated"
+                        block
+                        size="x-large"
+                        rounded="1"
+                        class="mx-auto">
+                        Cancel
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
 
 <style scoped></style>
