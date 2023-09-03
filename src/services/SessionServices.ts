2a9073969f028fb7765d6178bc7a747b74d42df9
metal-face <hughesbryan3000@gmail.com>
2023-09-03 03:12:20 -0400

    BUG FIX
    fetchSessionById had a typo in the route.

--- src/services/SessionServices.ts
+++ src/services/SessionServices.ts
@@ -1,4 +1,4 @@
-import ApiClient from "@/clients/ApiClient"
+import ApiClient from "@/clients/ApiClient";
 import { AxiosResponse } from "axios";
 
 export default {
@@ -10,31 +10,31 @@ export default {
                 username: username,
                 password: password,
                 token: token,
-            }
-        })
+            },
+        });
     },
     fetchManySessions(accountId?: string, user_agent?: string, page?: number, pageSize?: number) {
         return ApiClient({
             method: "GET",
             url: `/sessions?account_id=${accountId}&page=${page}&page_size=${pageSize}`,
             headers: {
-                "User-Agent": user_agent
-            }
-        })
+                "User-Agent": user_agent,
+            },
+        });
     },
     fetchSessionById(session_id: string): Promise<AxiosResponse<any, any>> {
         return ApiClient({
             method: "GET",
-            url: `/ssessions/${session_id}`
-        })
+            url: `/sessions/${session_id}`,
+        });
     },
     logout(sessionId: string): Promise<AxiosResponse<any, any>> {
         return ApiClient({
             method: "DELETE",
             url: `/sessions`,
             headers: {
-                "Authorization": `Bearer ${sessionId}`
-            }
-        })
-    }
-}
+                Authorization: `Bearer ${sessionId}`,
+            },
+        });
+    },
+};
