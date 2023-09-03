a309abcf10278e1e1e623a6d3b93044af4841502
metal-face <hughesbryan3000@gmail.com>
2023-09-03 00:36:13 -0400

    BUG FIX.
    Without a configuration block, the viewer cannot properly
    render without first visiting the editor.
    
    Passing in its own configuration allows it to render
    properly.

--- src/views/blogs/BlogViewer.vue
+++ src/views/blogs/BlogViewer.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
 import BlogServices from "@/services/BlogServices";
-import MarkdownEditor from "@/components/blogs/MarkdownEditor.vue";
 import { reactive, Ref, ref, onMounted, computed } from "vue";
 import { Router, useRouter } from "vue-router";
-import { useTheme, ThemeInstance } from "vuetify";
-import { MdPreview } from "md-editor-v3";
+import { useTheme, ThemeInstance, useDisplay, DisplayInstance } from "vuetify";
+import { MdPreview, MdCatalog, config } from "md-editor-v3";
+import { lineNumbers } from "@codemirror/view";
+import mermaid from "mermaid";
+import screenfull from "screenfull";
+import highlight from "highlight.js";
+import katex from "katex";
+import Cropper from "cropperjs";
+import "highlight.js/styles/github.css";
+import "cropperjs/dist/cropper.css";
+import "md-editor-v3/lib/style.css";
+import "katex/dist/katex.min.css";
 
 const router: Router = useRouter();
 const theme: ThemeInstance = useTheme();
@@ -19,9 +28,11 @@ interface Props {
     readonly?: string;
 }
 
+const display: DisplayInstance = useDisplay();
+
 const blog = reactive<Blog>({
-    blogPostTitle: ref<string>(""),
     blogPostBody: ref<string>(""),
+    blogPostTitle: ref<string>(""),
 });
 
 const darkState = computed<string>(() => {
@@ -36,10 +47,27 @@ const props = withDefaults(defineProps<Props>(), {
     readonly: "true",
 });
 
+const editorWidth = computed<number>(() => {
+    switch (display.name.value) {
+        case "xs":
+            return 12;
+        case "sm":
+            return 11;
+        case "md":
+            return 10;
+        case "lg":
+            return 10;
+        default:
+            return 10;
+    }
+});
+
+const scrollElement = document.documentElement;
+const id: string = "mdEditor";
+
 async function fetchBlogById() {
     BlogServices.fetchBlogById(props.blogId)
         .then((res) => {
-            console.log(res);
             blog.blogPostTitle = res.data.data.blog_title;
             blog.blogPostBody = res.data.data.blog_post;
         })
@@ -47,6 +75,30 @@ async function fetchBlogById() {
             console.error(err);
         });
 }
+
+// MDEditor configuration.
+config({
+    editorExtensions: {
+        highlight: {
+            instance: highlight,
+        },
+        mermaid: {
+            instance: mermaid,
+        },
+        screenfull: {
+            instance: screenfull,
+        },
+        katex: {
+            instance: katex,
+        },
+        cropper: {
+            instance: Cropper,
+        },
+    },
+    codeMirrorExtensions(_theme: any, extensions: any) {
+        return [...extensions, lineNumbers()];
+    },
+});
 
 onMounted(async () => {
     if (!props.blogId) return;
@@ -56,26 +108,42 @@ onMounted(async () => {
 
 <template>
     <v-row justify="center">
-        <v-col cols="6">
+        <v-col :cols="editorWidth">
             <v-card :color="isDark ? 'black' : 'white'" variant="elevated" width="100%">
+                <v-card-title class="editor text-center">
+                    <h1 class="ma-2">
+                        {{ blog.blogPostTitle }}
+                    </h1>
+                </v-card-title>
                 <MdPreview
-                    class="editor"
-                    id="mdEditor"
                     :modelValue="blog.blogPostBody"
-                    :theme="isDark ? 'dark' : 'light'" />
-                <v-card-actions class="ma-0 pa-0">
-                    <v-btn
-                        @click="router.push({ name: 'BlogPosts' })"
-                        :color="isDark ? 'accent' : 'black'"
-                        block
-                        variant="elevated"
-                        rounded="1"
-                        size="x-large">
-                        Go Back
-                    </v-btn>
-                </v-card-actions>
+                    :id="id"
+                    :theme="isDark ? 'dark' : 'light'"
+                    class="editor"
+                    show-code-row-number />
+                <MdCatalog
+                    :id="id"
+                    :theme="isDark ? 'dark' : 'light'"
+                    :scrollElement="scrollElement" />
             </v-card>
         </v-col>
+        <v-tooltip location="top">
+            <template #activator="{ props }">
+                <v-btn
+                    @click="router.push({ name: 'BlogPosts' })"
+                    :color="isDark ? 'accent' : 'black'"
+                    v-bind="props"
+                    position="fixed"
+                    location="bottom right"
+                    variant="elevated"
+                    rounded="5"
+                    class="mr-4 mb-4"
+                    icon="mdi-arrow-left"
+                    size="x-large">
+                </v-btn>
+            </template>
+            <span>Go Back</span>
+        </v-tooltip>
     </v-row>
 </template>
 
