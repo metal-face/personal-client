fd8e45720eebd5c7aa54c09662d280ba3d89c8c9
metal-face <hughesbryan3000@gmail.com>
2023-09-04 03:30:33 -0400

    Blog feed view started

---
+++ src/views/blogs/BlogFeed.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import BlogServices from "@/services/BlogServices";
+import { onMounted, reactive } from "vue";
+import { Blogs } from "@/models/Blogs";
+
+const blogs = reactive<Blogs>([]);
+
+async function fetchManyBlogs() {
+    return BlogServices.fetchManyBlogs()
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+}
+
+onMounted(async () => {
+    await fetchManyBlogs();
+});
+</script>
+
+<template>
+    <v-row>
+        <v-col>
+            <v-card>
+                <v-card-title> Your Feed </v-card-title>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+
+<style scoped></style>
