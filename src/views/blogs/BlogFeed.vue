b47dfc2392fac8414c5287e0d64cc8c253f5617b
metal-face <hughesbryan3000@gmail.com>
2023-09-05 02:24:02 -0400

    Blog feed built out

--- src/views/blogs/BlogFeed.vue
+++ src/views/blogs/BlogFeed.vue
@@ -1,14 +1,36 @@
 <script setup lang="ts">
 import BlogServices from "@/services/BlogServices";
-import { onMounted, reactive } from "vue";
+import { computed, onMounted, reactive, ref } from "vue";
 import { Blogs } from "@/models/Blogs";
+import { ThemeInstance, useTheme } from "vuetify";
+import { format } from "date-fns";
+import { useRouter, Router } from "vue-router";
+import GeneralBlogCard from "@/components/blogs/GeneralBlogCard.vue";
 
-const blogs = reactive<Blogs>([]);
+const theme: ThemeInstance = useTheme();
+const router: Router = useRouter();
+
+const blogPosts = ref<Blogs>({ blogs: [] });
+
+const isDark = computed<boolean>(() => {
+    return theme.current.value.dark;
+});
+
+function humanReadableDate(rawString: string): string {
+    console.log(rawString);
+    const rawDate: Date = new Date(rawString);
+    return format(rawDate, "PPPP");
+}
+
+function handleRedirection(blogId: string): void {
+    router.push({ name: "BlogViewer", params: { blogId: blogId, readonly: "true" } });
+}
 
 async function fetchManyBlogs() {
     return BlogServices.fetchManyBlogs()
         .then((res) => {
-            console.log(res);
+            blogPosts.value.blogs.splice(0, blogPosts.value.blogs.length);
+            blogPosts.value.blogs.push(...res.data.data);
         })
         .catch((err) => {
             console.error(err);
@@ -21,13 +43,33 @@ onMounted(async () => {
 </script>
 
 <template>
-    <v-row>
-        <v-col>
-            <v-card>
-                <v-card-title> Your Feed </v-card-title>
+    <v-row class="fill-height">
+        <v-col cols="12">
+            <v-card
+                class="fill-height"
+                variant="elevated"
+                elevation="6"
+                :color="isDark ? 'black' : 'white'">
+                <v-card-title class="text-center page-title text-decoration-underline">
+                    <h1 class="my-5">Your Feed</h1>
+                </v-card-title>
+                <v-card-text>
+                    <v-row>
+                        <v-col cols="4" v-for="blog in blogPosts.blogs" :key="blog.blog_id">
+                            <GeneralBlogCard
+                                @click="handleRedirection(blog.blog_id)"
+                                :blogPostTitle="blog.blog_title"
+                                :blogCreationDate="humanReadableDate(blog.created_at)" />
+                        </v-col>
+                    </v-row>
+                </v-card-text>
             </v-card>
         </v-col>
     </v-row>
 </template>
 
-<style scoped></style>
+<style scoped>
+.page-title {
+    font-family: "Prata", "serif";
+}
+</style>
