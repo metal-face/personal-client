ffabf8e6ad3f289ffe959d5fb9b983f4181188d1
metal-face <hughesbryan3000@gmail.com>
2023-09-05 02:24:13 -0400

    Blog model updated

--- src/models/Blog.ts
+++ src/models/Blog.ts
@@ -1,4 +1,8 @@
 export interface Blog {
-    blogPostTitle: string;
-    blogPostBody: string;
+    blog_post: string;
+    blog_title: string;
+    blog_id: string;
+    account_id: string;
+    created_at: string;
+    updated_at: string;
 }
