8b526644ac8c1a6b9a4132135bacbce784189bc4
metal-face <hughesbryan3000@gmail.com>
2023-09-05 02:23:30 -0400

    General blog card created

---
+++ src/components/blogs/GeneralBlogCard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+    blogPostTitle: string;
+    blogCreationDate: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+    <v-card variant="elevated" elevation="4">
+        <v-card-title>{{ props.blogPostTitle }}</v-card-title>
+        <v-card-subtitle>{{ props.blogCreationDate }}</v-card-subtitle>
+    </v-card>
+</template>
+
+<style scoped></style>
