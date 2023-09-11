c29e678c8f3333d636ef9c11a105a43c1ec8909a
metal-face <hughesbryan3000@gmail.com>
2023-09-11 19:55:35 -0400

    Redirection moved down to child component

--- src/components/blogs/GeneralBlogCard.vue
+++ src/components/blogs/GeneralBlogCard.vue
@@ -1,15 +1,29 @@
 <script setup lang="ts">
+import { computed } from "vue";
+import { Router, useRouter } from "vue-router";
+
 interface Props {
+    blogId: string;
     blogPostTitle: string;
     blogCreationDate: string;
 }
 
+const router: Router = useRouter();
+
 const props = defineProps<Props>();
+
+const blogId = computed<string>(() => {
+    return props.blogId;
+});
+
+function handleRedirection(blogId: string): void {
+    router.push({ name: "BlogViewer", params: { blogId: blogId, readonly: "true" } });
+}
 </script>
 
 <template>
     <v-card variant="elevated" elevation="4" class="blog-card pa-3">
-        <v-card-title class="mb-0 pa-0 text-center">
+        <v-card-title @click="handleRedirection(blogId)" class="card-title mb-0 pa-0 text-center">
             {{ props.blogPostTitle }}
         </v-card-title>
         <v-card-subtitle class="pb-0 text-center">
@@ -20,8 +34,12 @@ const props = defineProps<Props>();
 
 <style scoped>
 .blog-card:hover {
-    text-decoration: underline !important;
     transform: scale(1.02);
     transition: 200ms linear;
+}
+
+.card-title:hover {
+    text-decoration: underline !important;
+    cursor: pointer !important;
 }
 </style>
