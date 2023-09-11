f9c6e0a419644957a86929f702bc1317d5dd2004
metal-face <hughesbryan3000@gmail.com>
2023-09-11 02:03:28 -0400

    blog feed card layout made responsive

--- src/views/blogs/BlogFeed.vue
+++ src/views/blogs/BlogFeed.vue
@@ -2,7 +2,7 @@
 import BlogServices from "@/services/BlogServices";
 import { computed, onMounted, ref } from "vue";
 import { Blogs } from "@/models/Blogs";
-import { ThemeInstance, useTheme } from "vuetify";
+import { DisplayInstance, ThemeInstance, useDisplay, useTheme } from "vuetify";
 import { format } from "date-fns";
 import { useRouter, Router } from "vue-router";
 import GeneralBlogCard from "@/components/blogs/GeneralBlogCard.vue";
@@ -10,10 +10,13 @@ import CircleLoader from "@/components/utils/CircleLoader.vue";
 
 const theme: ThemeInstance = useTheme();
 const router: Router = useRouter();
+const display: DisplayInstance = useDisplay();
 
 const blogPosts = ref<Blogs>({ blogs: [] });
 
 const loading = ref<boolean>(false);
+
+const colCount = ref<number>(6);
 
 const isDark = computed<boolean>(() => {
     return theme.current.value.dark;
@@ -22,6 +25,28 @@ const isDark = computed<boolean>(() => {
 function humanReadableDate(rawString: string): string {
     const rawDate: Date = new Date(rawString);
     return format(rawDate, "PPPP");
+}
+
+function onResize() {
+    switch (display.name.value) {
+        case "xs":
+            colCount.value = 12;
+            break;
+        case "sm":
+            colCount.value = 8;
+            break;
+        case "md":
+            colCount.value = 6;
+            break;
+        case "lg":
+            colCount.value = 4;
+            break;
+        case "xl":
+            colCount.value = 3;
+            break;
+        default:
+            colCount.value = 6;
+    }
 }
 
 function handleRedirection(blogId: string): void {
@@ -47,20 +72,16 @@ onMounted(async () => {
 </script>
 
 <template>
-    <v-row class="fill-height">
-        <v-col cols="12">
+    <v-row class="fill-height" v-resize="onResize">
+        <v-col :cols="12">
             <CircleLoader :loading="loading" circle-color="pink" />
-            <v-card
-                class="fill-height"
-                variant="elevated"
-                elevation="6"
-                :color="isDark ? 'black' : 'white'">
+            <v-card class="fill-height" variant="flat" :color="isDark ? 'black' : 'white'">
                 <v-card-title class="text-center page-title text-decoration-underline">
                     <h1 class="my-5">Your Feed</h1>
                 </v-card-title>
                 <v-card-text>
-                    <v-row>
-                        <v-col cols="6" v-for="blog in blogPosts.blogs" :key="blog.blog_id">
+                    <v-row justify="center" align-content="center">
+                        <v-col :cols="colCount" v-for="blog in blogPosts.blogs" :key="blog.blog_id">
                             <GeneralBlogCard
                                 @click="handleRedirection(blog.blog_id)"
                                 :blogPostTitle="blog.blog_title"
