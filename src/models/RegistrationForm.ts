8a52b205fd3104f5412787cb5e0467c9e05bdd10
metal-face <hughesbryan3000@gmail.com>
2023-09-14 00:52:15 -0400

    Several model definitions moved out of login

---
+++ src/models/RegistrationForm.ts
@@ -0,0 +1,5 @@
+export interface RegistrationForm {
+    email: string;
+    username: string;
+    password: string;
+}
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,108 +1,58 @@
-<template>
-    <v-row dense no-gutters justify="center">
-        <CircleLoader :loading="loading" circleColor="black" />
-        <v-col :cols="$vuetify.display.mobile ? 12 : 6">
-            <v-card height="100%" width="90%" class="mx-auto" flat variant="outlined">
-                <v-card-title class="text-center">
-                    <h2 class="form-title ma-3">Register</h2>
-                </v-card-title>
-                <form id="registrationForm">
-                    <v-container fluid>
-                        <v-row dense>
-                            <v-col cols="12">
-                                <v-alert
-                                    v-model="alert.visible"
-                                    :title="alert.title"
-                                    :text="alert.text"
-                                    :type="alert.type"
-                                    :color="alert.color"
-                                    closable />
-                            </v-col>
-                            <v-col cols="12">
-                                <v-text-field
-                                    v-model="state.email"
-                                    @input="v$.email.$touch"
-                                    @blur="v$.email.$touch"
-                                    :error-messages="
-                                        v$.email.$errors.map((e) => e.$message.toString())
-                                    "
-                                    required
-                                    variant="outlined"
-                                    label="Email" />
-                            </v-col>
-                            <v-col cols="12">
-                                <v-text-field
-                                    v-model="state.username"
-                                    @input="v$.username.$touch"
-                                    @blur="v$.username.$touch"
-                                    :error-messages="
-                                        v$.username.$errors.map((e) => e.$message.toString())
-                                    "
-                                    required
-                                    label="Username"
-                                    variant="outlined" />
-                            </v-col>
-                            <v-col cols="12">
-                                <v-text-field
-                                    v-model="state.password"
-                                    @click:append-inner="toggleVisibility"
-                                    @input="v$.password.$touch"
-                                    @blur="v$.password.$touch"
-                                    :error-messages="
-                                        v$.password.$errors.map((e) => e.$message.toString())
-                                    "
-                                    :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-                                    :type="visible ? 'text' : 'password'"
-                                    required
-                                    variant="outlined"
-                                    label="Password" />
-                            </v-col>
-                        </v-row>
-                    </v-container>
-                </form>
-                <v-card-actions class="d-flex flex-column align-center justify-end">
-                    <v-btn
-                        @click="onClick"
-                        block
-                        :disabled="disabled"
-                        size="x-large"
-                        color="accent"
-                        variant="elevated"
-                        rounded="false"
-                        class="ma-1">
-                        Register
-                    </v-btn>
-                    <v-btn
-                        @click="router.push({ name: 'Home' })"
-                        variant="flat"
-                        block
-                        color="primary"
-                        size="x-large"
-                        rounded="false"
-                        class="ma-1">
-                        Cancle
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-col>
-    </v-row>
-</template>
 <script setup lang="ts">
-import { ref, reactive, onMounted, defineEmits } from "vue";
+import { ref, reactive, onMounted, defineEmits, computed, Ref } from "vue";
 import { Router, useRouter } from "vue-router";
-import { useVuelidate } from "@vuelidate/core";
+import { useVuelidate, Validation } from "@vuelidate/core";
 import { email, required, minLength, maxLength } from "@vuelidate/validators";
 import AccountsServices from "@/services/AccountsServices";
 import CircleLoader from "@/components/utils/CircleLoader.vue";
 import SessionServices from "@/services/SessionServices";
 import { sessionStore } from "@/store/SessionStore";
-import type { AxiosResponse } from "axios";
 import { useScriptTag } from "@vueuse/core";
+import { Alert } from "@/models/Alert";
+import { AlertTypes } from "@/models/AlertTypes";
+import { ColorTypes } from "@/models/ColorTypes";
+import { RegistrationForm } from "@/models/RegistrationForm";
+
+onMounted(() => {
+    clearForm();
+});
 
 // Load Google Recaptcha
 useScriptTag(
     "https://www.google.com/recaptcha/api.js?render=6Ldz_0snAAAAAEDnmEgNJgFAB2zWkOod_QJijLMM",
 );
+
+const router: Router = useRouter();
+const session = sessionStore();
+
+const alert: Alert = reactive({
+    type: AlertTypes.success,
+    color: ColorTypes.success,
+    title: "",
+    text: "",
+    visible: false,
+    timeout: 0,
+});
+
+const state: RegistrationForm = reactive({ email: "", username: "", password: "" });
+
+const rules = computed<object>(() => ({
+    email: { required, email },
+    username: { required, minLength: minLength(3), maxLength: maxLength(16) },
+    password: { required, minLength: minLength(8), maxLength: maxLength(128) },
+}));
+
+const v$: Ref<Validation<object, RegistrationForm>> = useVuelidate(rules, state);
+
+const visible = ref<boolean>(false);
+
+const loading = ref<boolean>(false);
+
+const disabled = ref<boolean>(false);
+
+const emit = defineEmits<{
+    (e: "username:update", username: string): void;
+}>();
 
 function onClick(e: Event) {
     e.preventDefault();
@@ -116,71 +66,6 @@ function onClick(e: Event) {
             });
     });
 }
-
-const router: Router = useRouter();
-const session = sessionStore();
-
-interface RegistrationForm {
-    email: string;
-    username: string;
-    password: string;
-}
-
-interface Alert {
-    type: AlertTypes;
-    color: ColorTypes;
-    title: string;
-    text: string;
-    visible: boolean;
-    timeout: number;
-}
-
-enum AlertTypes {
-    success = "success",
-    info = "info",
-    warning = "warning",
-    error = "error",
-}
-
-enum ColorTypes {
-    success = "success",
-    info = "info",
-    warning = "warning",
-    error = "error",
-}
-
-const alert: Alert = reactive({
-    type: AlertTypes.success,
-    color: ColorTypes.success,
-    title: "",
-    text: "",
-    visible: false,
-    timeout: 0,
-});
-
-const state: RegistrationForm = reactive({ email: "", username: "", password: "" });
-
-const rules = {
-    email: { required, email },
-    username: { required, minLength: minLength(3), maxLength: maxLength(16) },
-    password: { required, minLength: minLength(8), maxLength: maxLength(128) },
-};
-
-const v$ = useVuelidate(rules, state);
-
-const visible = ref<boolean>(false);
-
-const loading = ref<boolean>(false);
-
-const disabled = ref<boolean>(false);
-
-const emit = defineEmits<{
-    (e: "username:update", username: string): void;
-}>();
-
-onMounted(() => {
-    clearForm();
-});
 
 function toggleVisibility() {
     visible.value = !visible.value;
@@ -247,7 +132,6 @@ async function loginUser(token: string): Promise<boolean> {
             session.setSession(res.data.data);
             emit("username:update", state.username);
             loading.value = false;
-
             return true;
         })
         .catch((err) => {
@@ -262,6 +146,95 @@ async function loginUser(token: string): Promise<boolean> {
         });
 }
 </script>
+<template>
+    <v-row dense no-gutters justify="center">
+        <CircleLoader :loading="loading" circleColor="black" />
+        <v-col :cols="$vuetify.display.mobile ? 12 : 6">
+            <v-card height="100%" width="90%" class="mx-auto" flat variant="outlined">
+                <v-card-title class="text-center">
+                    <h2 class="form-title ma-3">Register</h2>
+                </v-card-title>
+                <form id="registrationForm">
+                    <v-container fluid>
+                        <v-row dense>
+                            <v-col cols="12">
+                                <v-alert
+                                    v-model="alert.visible"
+                                    :title="alert.title"
+                                    :text="alert.text"
+                                    :type="alert.type"
+                                    :color="alert.color"
+                                    closable />
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field
+                                    v-model="state.email"
+                                    @input="v$['email'].$touch"
+                                    @blur="v$['email'].$touch"
+                                    :error-messages="
+                                        v$['email'].$errors.map((e) => e.$message.toString())
+                                    "
+                                    required
+                                    variant="outlined"
+                                    label="Email" />
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field
+                                    v-model="state.username"
+                                    @input="v$['username'].$touch"
+                                    @blur="v$['username'].$touch"
+                                    :error-messages="
+                                        v$['username'].$errors.map((e) => e.$message.toString())
+                                    "
+                                    required
+                                    label="Username"
+                                    variant="outlined" />
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field
+                                    v-model="state.password"
+                                    @click:append-inner="toggleVisibility"
+                                    @input="v$['password'].$touch"
+                                    @blur="v$['password'].$touch"
+                                    :error-messages="
+                                        v$['password'].$errors.map((e) => e.$message.toString())
+                                    "
+                                    :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+                                    :type="visible ? 'text' : 'password'"
+                                    required
+                                    variant="outlined"
+                                    label="Password" />
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </form>
+                <v-card-actions class="d-flex flex-column align-center justify-end">
+                    <v-btn
+                        @click="onClick"
+                        block
+                        :disabled="disabled"
+                        size="x-large"
+                        color="accent"
+                        variant="elevated"
+                        rounded="false"
+                        class="ma-1">
+                        Register
+                    </v-btn>
+                    <v-btn
+                        @click="router.push({ name: 'Home' })"
+                        variant="flat"
+                        block
+                        color="primary"
+                        size="x-large"
+                        rounded="false"
+                        class="ma-1">
+                        Cancle
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
 
 <style scoped>
 .form-title {
