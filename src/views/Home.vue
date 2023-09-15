39ea1c158f7e04316b85ec09609364b3f7b65fb2
metal-face <hughesbryan3000@gmail.com>
2023-09-15 03:41:11 -0400

    home refactored to be more explanatory

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
 import { ref, reactive, computed, ComputedRef } from "vue";
 import { useAccountStore } from "@/store/AccountStore";
+import { Snackbar } from "@/models/Snackbar";
 import PersonalButtonGroup from "@/components/home/PersonalButtonGroup.vue";
 import AuthenticationButtons from "@/components/home/AuthenticationButtons.vue";
 import PersonalInfoCard from "@/components/home/PersonalInfoCard.vue";
 import CircleLoader from "@/components/utils/CircleLoader.vue";
-import { Snackbar } from "@/models/Snackbar";
+import MarkdownEditor from "@/components/blogs/MarkdownEditor.vue";
+import { DisplayInstance, useDisplay } from "vuetify";
 
 const accountStore = useAccountStore();
 
 const loggedIn: ComputedRef<boolean> = computed(() => {
     return accountStore.isLoggedIn;
 });
+
+const display: DisplayInstance = useDisplay();
 
 const snackbar: Snackbar = reactive({
     visible: false,
@@ -22,30 +26,58 @@ const snackbar: Snackbar = reactive({
 
 const loading = ref<boolean>(true);
 
+const colCount = ref<number>(0);
+
 function toggleLoadingState(state: boolean): void {
     loading.value = state;
+}
+
+function onResize(): void {
+    switch (display.name.value) {
+        case "xs":
+            colCount.value = 12;
+            break;
+        case "sm":
+            colCount.value = 10;
+            break;
+        case "md":
+            colCount.value = 8;
+            break;
+        case "lg":
+            colCount.value = 6;
+            break;
+        case "xl":
+            colCount.value = 6;
+            break;
+    }
 }
 </script>
 
 <template>
-    <v-row class="fill-height">
-        <v-col cols="12">
-            <v-card
-                flat
-                color="transparent"
-                class="fill-height d-flex flex-column justify-center align-center"
-                width="100%">
-                <v-card color="transparent" flat class="ma-5">
-                    <CircleLoader :loading="loading" circle-color="pink" />
-
-                    <PersonalInfoCard @loading="toggleLoadingState" />
-                    <AuthenticationButtons v-if="!loggedIn" class="ma-6" />
-                    <PersonalButtonGroup v-if="loggedIn" />
+    <v-container fluid class="pa-0">
+        <v-row v-resize="onResize" class="fill-height" justify="center">
+            <v-col cols="12">
+                <v-card
+                    flat
+                    color="transparent"
+                    class="d-flex flex-column justify-start align-center"
+                    width="100%">
+                    <v-card color="transparent" flat class="ma-0">
+                        <CircleLoader :loading="loading" circle-color="accent" />
+                        <PersonalInfoCard @loading="toggleLoadingState" />
+                        <AuthenticationButtons v-if="!loggedIn" class="ma-1" />
+                        <PersonalButtonGroup v-if="loggedIn" />
+                    </v-card>
                 </v-card>
-            </v-card>
-            <v-snackbar v-model="snackbar.visible" :timeout="snackbar.timeout">
-                {{ snackbar.text }}
-            </v-snackbar>
-        </v-col>
-    </v-row>
+                <v-snackbar v-model="snackbar.visible" :timeout="snackbar.timeout">
+                    {{ snackbar.text }}
+                </v-snackbar>
+            </v-col>
+            <v-col :cols="colCount">
+                <v-card variant="elevated" elevation="8">
+                    <MarkdownEditor :demo="true" />
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
