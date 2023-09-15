21a6d947913a993004c965aa95a12e255438473d
metal-face <hughesbryan3000@gmail.com>
2023-09-15 03:41:48 -0400

    content completely changed to be more informative of sites purpose

--- src/components/home/PersonalInfoCard.vue
+++ src/components/home/PersonalInfoCard.vue
@@ -1,11 +1,16 @@
 <template>
-    <v-card-title class="page-title ma-1">
-        <h1 class="page-title text-center">MetalFace</h1>
-    </v-card-title>
-    <v-card-subtitle class="page-clock ma-1">
-        <h3 class="text-center">Full-Stack Software Developer</h3>
-        <h3 class="text-center page-clock ma-1">{{ templateDate }}</h3>
-    </v-card-subtitle>
+    <v-card variant="flat" color="transparent">
+        <v-card-title
+            class="page-title my-1 py-lg-6 py-md-3 py-sm-1 py-0 text-xxl-h2 text-lg-h3 text-md-h4 text-sm-h4 text-h5 text-xl-h3 text-center">
+            Build Your Own Blogs
+        </v-card-title>
+        <v-card-text class="text-center page-clock text-body-2 text-sm-h6 ma-1 pa-1">
+            {{ templateDate }}
+        </v-card-text>
+        <v-card-text class="page-clock text-center text-body-1 text-sm-h6 ma-1 pa-1">
+            Built with ❤️ by MetalFace
+        </v-card-text> </v-card
+    >️
 </template>
 <script setup lang="ts">
 import { format } from "date-fns";
