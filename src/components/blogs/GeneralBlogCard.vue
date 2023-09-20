950ce067870eb7b7785439bf7f71d122f2264886
metal-face <hughesbryan3000@gmail.com>
2023-09-20 19:48:45 -0400

    Improvements
    Ability to fetch account by id added.
    On mount of component, account info
    is fetched for the author of the blog
    and then presented in the template.
    
    Control of parents loading state
    added.

--- src/components/blogs/GeneralBlogCard.vue
+++ src/components/blogs/GeneralBlogCard.vue
@@ -1,20 +1,75 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, onMounted, reactive } from "vue";
 import { Router, useRouter } from "vue-router";
+import { Account, Role } from "@/models/Account";
+import AccountsServices from "@/services/AccountsServices";
+
+onMounted(async () => {
+    emit("loading", true);
+    await fetchAccountById();
+    emit("loading", false);
+});
 
 interface Props {
+    account_id: string;
     blogId: string;
     blogPostTitle: string;
     blogCreationDate: string;
 }
 
+const emit = defineEmits<{
+    (event: "loading", value: boolean): void;
+}>();
+
 const router: Router = useRouter();
 
 const props = defineProps<Props>();
 
+const account = reactive<Account>({
+    account_id: "",
+    username: "",
+    email: "",
+    role: Role.REGULAR,
+    created_at: new Date(),
+});
+
 const blogId = computed<string>(() => {
     return props.blogId;
 });
+
+const accountId = computed<string>(() => {
+    return props.account_id;
+});
+
+async function fetchAccountById(): Promise<boolean> {
+    const res = await AccountsServices.fetchAccountById(accountId.value);
+
+    if (!res) {
+        return false;
+    }
+
+    account.account_id = res.data.data.account_id;
+
+    switch (res.data.data.role) {
+        case "REGULAR":
+            account.role = Role.REGULAR;
+            break;
+        case "SUPER":
+            account.role = Role.SUPER;
+            break;
+        case "ADMIN":
+            account.role = Role.ADMIN;
+            break;
+        default:
+            account.role = Role.REGULAR;
+            break;
+    }
+
+    account.username = res.data.data.username;
+    account.created_at = new Date(res.data?.data?.created_at);
+
+    return true;
+}
 
 function handleRedirection(blogId: string): void {
     router.push({ name: "BlogViewer", params: { blogId: blogId, readonly: "true" } });
@@ -23,6 +78,9 @@ function handleRedirection(blogId: string): void {
 
 <template>
     <v-card variant="elevated" elevation="4" class="blog-card pa-3 ma-3">
+        <v-card-subtitle class="d-flex justify-end align-center">
+            {{ account.username }}
+        </v-card-subtitle>
         <v-card-title @click="handleRedirection(blogId)" class="card-title mb-0 pa-0 text-center">
             {{ props.blogPostTitle }}
         </v-card-title>
