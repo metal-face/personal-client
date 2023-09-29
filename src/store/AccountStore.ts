cfb954e88557f2b22aa00e7d721c7c0de3e58d49
metal-face <hughesbryan3000@gmail.com>
2023-09-29 01:54:55 -0400

    function to determine error added

--- src/store/AccountStore.ts
+++ src/store/AccountStore.ts
@@ -6,6 +6,8 @@ interface State {
     blankAccount: Account;
 }
 
+// TODO: REFACTOR TO USE COMPOSITION API
+
 export const useAccountStore = defineStore("account", {
     state: (): State => {
         return {
@@ -31,6 +33,30 @@ export const useAccountStore = defineStore("account", {
         },
         clearAccount(): void {
             Object.assign(this.account, this.blankAccount);
+        },
+        determineFailureReason(rawReason: string): string {
+            switch (rawReason) {
+                case "accounts.signup_failed":
+                    return "Registration has failed.";
+                case "accounts.deletion_failed":
+                    return "Failed to delete this account!";
+                case "accounts.not_found":
+                    return "Account could not be found!";
+                case "accounts.username_invalid":
+                    return "The username you submitted is invalid!";
+                case "accounts.password_invalid":
+                    return "The password you submitted is invalid";
+                case "accounts.email_not_found":
+                    return "Email could not be found!";
+                case "accounts.email_address_invalid":
+                    return "The email you submitted is invalid";
+                case "accounts.email_address_exists":
+                    return "An account with that email already exists!";
+                case "accounts.username_exists":
+                    return "Username already exists!";
+                default:
+                    return "Error! Registration failure!";
+            }
         },
     },
     getters: {
