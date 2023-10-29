42ce312283aace206dbe2574ccc9420d0a59b7e5
metal-face <hughesbryan3000@gmail.com>
2023-10-29 03:08:06 -0400

    Session Store Completely Refactored.
    Previously, the session store was utilizing Vue's
    option API, it has now been refactored to use
    the composition API.

--- src/store/SessionStore.ts
+++ src/store/SessionStore.ts
@@ -1,92 +1,71 @@
 // Utilities
 import { defineStore } from "pinia";
 import { Session } from "@/models/Session";
-import SessionServices from "@/services/SessionServices";
-import { AxiosResponse } from "axios";
+import { computed, ComputedRef, Ref, ref } from "vue";
 
-interface State {
-    session: Session;
-    emptySession: Session;
-}
+export const sessionStore = defineStore("session", () => {
+    const session: Ref<Session> = ref<Session>({
+        account_id: "",
+        session_id: "",
+        created_at: new Date(),
+        expires_at: new Date(),
+        user_agent: "",
+    });
 
-export const sessionStore = defineStore("session", {
-    state: (): State => {
-        return {
-            session: {
-                account_id: "",
-                session_id: "",
-                created_at: new Date(),
-                expires_at: new Date(),
-                user_agent: "",
-            },
-            emptySession: {
-                account_id: "",
-                session_id: "",
-                created_at: new Date(),
-                expires_at: new Date(),
-                user_agent: "",
-            },
-        };
-    },
-    actions: {
-        setSession(session: Session): void {
-            window.localStorage.setItem("session", JSON.stringify(session));
-            Object.assign(this.session, session);
-        },
-        destroySessionInStorage(): void {
-            window.localStorage.removeItem("session");
-        },
-        clearSession(): void {
-            Object.assign(this.session, this.emptySession);
-        },
-    },
-    getters: {
-        getSession(state: State): Session {
-            return state.session;
-        },
-        isLoggedIn(state: State): boolean {
-            return state.session.account_id !== "";
-        },
-        isAuthenticated: async function (state: State): Promise<boolean> {
-            const rawSession = window.localStorage.getItem("session");
+    const blankSession: Ref<Session> = ref<Session>({
+        account_id: "",
+        session_id: "",
+        created_at: new Date(),
+        expires_at: new Date(),
+        user_agent: "",
+    });
 
-            if (!rawSession) return false;
+    function setSession(payload: Session): void {
+        window.localStorage.setItem("session", JSON.stringify(payload));
+        session.value = payload;
+    }
 
-            const jsonSession: Session = JSON.parse(rawSession) as Session;
-            const sessionId: string = jsonSession.session_id;
+    function clearSession(): void {
+        session.value = blankSession.value;
+    }
 
-            if (sessionId) {
-                const res: AxiosResponse<any, any> = await SessionServices.fetchSessionById(
-                    sessionId,
-                );
+    function destroySessionInStorage(): void {
+        window.localStorage.removeItem("session");
+    }
 
-                if (res.data.data.session_id && res.data.data.expires_at) {
-                    const expiresAt: Date = new Date(res.data.data.expires_at);
-                    const now: Date = new Date();
+    const getSession: ComputedRef<Session> = computed<Session>(() => {
+        return session.value;
+    });
 
-                    if (expiresAt > now) {
-                        Object.assign(state.session, res.data.data as Session);
-                        return true;
-                    }
-                }
-                return false;
-            }
-            return false;
-        },
-        isExpired(state: State): boolean {
-            return state.session.expires_at < new Date();
-        },
-        getSessionId(state: State): string {
-            return state.session.session_id;
-        },
-        createdAt(state: State): Date {
-            return state.session.created_at;
-        },
-        expiresAt(state: State): Date {
-            return state.session.expires_at;
-        },
-        accountID(state: State): string {
-            return state.session.account_id;
-        },
-    },
+    const isLoggedIn: ComputedRef<boolean> = computed<boolean>(() => {
+        return session.value.account_id !== "";
+    });
+
+    const isExpired: ComputedRef<boolean> = computed<boolean>(() => {
+        return session.value.expires_at < new Date();
+    });
+
+    const getSessionId: ComputedRef<string> = computed<string>(() => {
+        return session.value.session_id;
+    });
+
+    const expiresAt: ComputedRef<Date> = computed<Date>(() => {
+        return session.value.expires_at;
+    });
+
+    const accountID: ComputedRef<string> = computed<string>(() => {
+        return session.value.account_id;
+    });
+
+    return {
+        setSession,
+        clearSession,
+        destroySessionInStorage,
+        getSession,
+        isLoggedIn,
+        isExpired,
+        getSessionId,
+        expiresAt,
+        accountID,
+    };
 });
