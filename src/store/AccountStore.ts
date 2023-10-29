9c6820ad50507806bbfbb717ea08f07ee59764ce
metal-face <hughesbryan3000@gmail.com>
2023-10-29 02:35:07 -0400

    Account Store Completely Refactored
    The account store was previously using the Vue
    options API. It has been refactored to use the
    composition API.

--- src/store/AccountStore.ts
+++ src/store/AccountStore.ts
@@ -1,79 +1,77 @@
 import { defineStore } from "pinia";
 import { Account, Role } from "@/models/Account";
+import { computed, ComputedRef, Ref, ref } from "vue";
 
-interface State {
-    account: Account;
-    blankAccount: Account;
-}
+export const useAccountStore = defineStore("account", () => {
+    const account: Ref<Account> = ref<Account>({
+        account_id: "",
+        username: "",
+        role: Role.REGULAR,
+        email: "",
+        created_at: new Date(),
+    });
 
-// TODO: REFACTOR TO USE COMPOSITION API
+    const blankAccount: Ref<Account> = ref<Account>({
+        account_id: "",
+        username: "",
+        role: Role.REGULAR,
+        email: "",
+        created_at: new Date(),
+    });
 
-export const useAccountStore = defineStore("account", {
-    state: (): State => {
-        return {
-            account: {
-                account_id: "",
-                username: "",
-                role: Role.REGULAR,
-                email: "",
-                created_at: new Date(),
-            },
-            blankAccount: {
-                account_id: "",
-                username: "",
-                role: Role.REGULAR,
-                email: "",
-                created_at: new Date(),
-            },
-        };
-    },
-    actions: {
-        setAccount(account: Account) {
-            Object.assign(this.account, account);
-        },
-        clearAccount(): void {
-            Object.assign(this.account, this.blankAccount);
-        },
-        determineFailureReason(rawReason: string): string {
-            switch (rawReason) {
-                case "accounts.signup_failed":
-                    return "Registration has failed.";
-                case "accounts.deletion_failed":
-                    return "Failed to delete this account!";
-                case "accounts.not_found":
-                    return "Account could not be found!";
-                case "accounts.username_invalid":
-                    return "The username you submitted is invalid!";
-                case "accounts.password_invalid":
-                    return "The password you submitted is invalid";
-                case "accounts.email_not_found":
-                    return "Email could not be found!";
-                case "accounts.email_address_invalid":
-                    return "The email you submitted is invalid";
-                case "accounts.email_address_exists":
-                    return "An account with that email already exists!";
-                case "accounts.username_exists":
-                    return "Username already exists!";
-                default:
-                    return "Error! Registration failure!";
-            }
-        },
-    },
-    getters: {
-        isAdmin: (state: State) => {
-            return state.account.role === Role.ADMIN;
-        },
-        isSuper: (state: State) => {
-            return state.account.role === Role.SUPER;
-        },
-        isRegular: (state: State) => {
-            return state.account.role === Role.REGULAR;
-        },
-        isLoggedIn: (state: State) => {
-            return state.account.account_id !== "";
-        },
-        getAccount: (state: State) => {
-            return state.account;
-        },
-    },
+    function setAccount(payload: Account): void {
+        account.value = payload;
+    }
+
+    function clearAccount(): void {
+        account.value = blankAccount.value;
+    }
+
+    function determineFailureReason(rawReason: string): string {
+        switch (rawReason) {
+            case "accounts.signup_failed":
+                return "Registration has failed.";
+            case "accounts.deletion_failed":
+                return "Failed to delete this account!";
+            case "accounts.not_found":
+                return "Account could not be found!";
+            case "accounts.username_invalid":
+                return "The username you submitted is invalid!";
+            case "accounts.password_invalid":
+                return "The password you submitted is invalid";
+            case "accounts.email_not_found":
+                return "Email could not be found!";
+            case "accounts.email_address_invalid":
+                return "The email you submitted is invalid";
+            case "accounts.email_address_exists":
+                return "An account with that email already exists!";
+            case "accounts.username_exists":
+                return "Username already exists!";
+            default:
+                return "Error! Registration failure!";
+        }
+    }
+
+    const isAdmin: ComputedRef<boolean> = computed<boolean>(() => {
+        return account.value.role === Role.ADMIN;
+    });
+
+    const getAccount: ComputedRef<Account> = computed<Account>(() => {
+        return account.value;
+    });
+
+    const isLoggedIn: ComputedRef<boolean> = computed<boolean>(() => {
+        return account.value.account_id !== "";
+    });
+
+    return {
+        account,
+        blankAccount,
+        setAccount,
+        clearAccount,
+        determineFailureReason,
+        isAdmin,
+        getAccount,
+        isLoggedIn,
+    };
 });
