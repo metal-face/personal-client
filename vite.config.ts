90c6537b84055813c75d908fe161ccce6e652ad5
metal-face <hughesbryan3000@gmail.com>
2023-11-08 16:40:05 -0500

    manual chunking added

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
 import svgLoader from "vite-svg-loader";
+import { splitVendorChunkPlugin } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,13 +19,27 @@ export default defineConfig({
             autoImport: true,
         }),
         svgLoader(),
+        splitVendorChunkPlugin(),
     ],
+
     define: { "process.env": {} },
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
         extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vuetify: ["vuetify"],
+                    highlight: ["highlight.js"],
+                    mermaid: ["mermaid"],
+                    cropperjs: ["cropperjs"],
+                },
+            },
+        },
     },
     server: {
         cors: { origin: true },
